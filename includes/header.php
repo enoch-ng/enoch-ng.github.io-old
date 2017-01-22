398b852a40f3c05a0e81e3e570ea1504bb959ea5
Enoch Ng <ewnoch@gmail.com>
2017-01-22 14:02:47 -0600

    Fixed paths in links, removed unnecessary stuff

--- includes/header.php
+++
@@ -1,2 +0,0 @@
-<h1>Enoch Ng</h1>
-<p>Music • Writing • Programming</p>
--- includes/nav.php
+++
@@ -1,8 +0,0 @@
-<ol>
-	<li><a href="http://enochngportfolio.x10host.com/about/">About</a></li>
-	<li><a href="http://enochngportfolio.x10host.com/projects/">Projects</a></li>
-	<li><a href="http://enochngportfolio.x10host.com/compositions/">Compositions</a></li>
-	<li><a href="http://enochngportfolio.x10host.com/arrangements/">Arrangements</a></li>
-	<li><a href="http://enochngportfolio.x10host.com/essays/">Essays</a></li>
-	<li><a href="http://enochngportfolio.x10host.com/contact/">Contact</a></li>
-</ol>
